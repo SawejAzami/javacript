e2ae2828ec5655e4bc10b484060d9b429da372d2
SawejAzami <122581170+SawejAzami@users.noreply.github.com>
2023-07-26 13:28:41 +0000

    I have done it

---
+++ lecture5.js
@@ -0,0 +1,12 @@
+// datatype conversion
+
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2=1);
+// console.log(2==1);
+// console.log(2<1);
+// these type of conversion we can easly predict
+  
+console.log("2">1);
+console.log("2"=1);
+// these type of conversion is not easy to predict
