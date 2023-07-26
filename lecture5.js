9b58b7c64a2fb77be6ec9fdf7b133d88aa427db7
SawejAzami <122581170+SawejAzami@users.noreply.github.com>
2023-07-26 14:39:10 +0000

    compairson of datatype

--- lecture5.js
+++ lecture5.js
@@ -7,6 +7,18 @@
 // console.log(2<1);
 // these type of conversion we can easly predict
   
-console.log("2">1);
-console.log("2"=1);
+// console.log("2">1);
+// console.log("2">=1);
 // these type of conversion is not easy to predict
+console.log(null > 0); //=> false
+console.log(null == 0); //=> false
+console.log(null >= 0); //=> true
+
+console.log(undefined == 0); //=> false
+
+// == it is use to compare in javascript
+// === it it is use to compare their value and datatype
+ 
+console.log("2"===2);//=>false
+console.log("2"==2);//=>true
+
