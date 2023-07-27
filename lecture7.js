ce695d91e27b0469bd9fbde2ceb415a3cb9a9662
SawejAzami <122581170+SawejAzami@users.noreply.github.com>
2023-07-27 12:23:54 +0000

    string

--- lecture7.js
+++ lecture7.js
@@ -12,3 +12,25 @@ console.log(name1); //=> [String: 'Hitesh sir']
 // there are various method in string,
 //like- split,slice,trim,toUpperCase,includes,charAt etc.
 
+let str="webDevelopment with javascript";
+console.log(str.charAt(3)); //=>D    it give the letter at the given index
+
+console.log(str.indexOf('w')); //=>0 it give the index of the given letter
+
+console.log(str.trim()); //=> it remove the extra spaces int the sentence
+
+console.log(str.replace("javascript","html")); //=>webDevelopment with html,
+// it replece the first word with the second word
+
+console.log(str.slice(3,8)); //=>Devel,   it give the subsrting whitin given position  
+
+console.log(str.split(' ')); //=>[ 'webDevelopment', 'with', 'javascript' ]
+// it it split the given sentence according to the given space,-,_,' '," "  etc
+
+console.log(str.toUpperCase());//=>WEBDEVELOPMENT WITH JAVASCRIPT
+
+console.log(str.includes("css"));// false
+
+let name2="shyam";
+let score=50;
+console.log('hello my name is ${name2} and I have scored ${score} in my exam');
