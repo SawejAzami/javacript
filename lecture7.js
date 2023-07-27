0f7bd35c7a4eb62c898c8ef7d6b0bf408a5a970c
SawejAzami <122581170+SawejAzami@users.noreply.github.com>
2023-07-27 05:19:13 +0000

    String

---
+++ lecture7.js
@@ -0,0 +1,14 @@
+// String
+
+// how will you define a sring, lets see.
+ 
+let name="hitesh sir";
+//or
+let name1=new String("Hitesh sir")
+console.log(name);  //=>hitesh sir
+console.log(name1); //=> [String: 'Hitesh sir']
+
+
+// there are various method in string,
+//like- split,slice,trim,toUpperCase,includes,charAt etc.
+
