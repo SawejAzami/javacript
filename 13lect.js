370f1858057b587fba4cc9cdc3d95f2f18bfbc5d
SawejAzami <122581170+SawejAzami@users.noreply.github.com>
2023-07-31 02:36:40 +0000

    global and local scope

---
+++ 13lect.js
@@ -0,0 +1,20 @@
+  // ************* Global and Local Scope  **********
+   
+  //Block scope: The scope created with a pair of curly braces (a block).
+//  Global scope: The default scope for all code running in script mode.
+  
+// let a=10;
+// const b=20;
+// var c=30;
+// console.log(a);  //10
+// console.log(b);  //20
+// console.log(c);  //30 
+
+{
+    let a=10;
+const b=20;
+var c=30;
+}
+console.log(a);  // it give error
+console.log(b);  // it give error
+console.log(c);  //30 
