2138646623eeef64af404200789233d00e802af6
SawejAzami <122581170+SawejAzami@users.noreply.github.com>
2023-08-12 09:53:17 +0000

    global and local scope

--- 13lect.js
+++ 13lect.js
@@ -10,11 +10,43 @@
 // console.log(b);  //20
 // console.log(c);  //30 
 
-{
-    let a=10;
-const b=20;
-var c=30;
+// {
+//     let a=10;
+// const b=20;
+// var c=30;
+// }
+// console.log(a);  // it give error
+// console.log(b);  // it give error
+// console.log(c);  //30 
+
+
+// function scope
+
+function one(){
+  const user="hitesh sir";
+       function two(){
+          const website="pw skills";
+          console.log(user);
+       }
+       two();
+      
 }
-console.log(a);  // it give error
-console.log(b);  // it give error
-console.log(c);  //30 
+one();
+
+// function two() can use the value of the function one() but function one() can not use value of function two()
+
+
+console.log(addone(5));
+function addone(num){
+  return num +1;
+}
+
+
+//console.log(addtwo);    //Cannot access 'addtwo' before initialization  because function is asign to a variable
+const addtwo=function(num){  // addtwo is know as expression
+  return num+2;
+}
+
+// *** hoisting  => you can not access the variable before declaratin
+
+
