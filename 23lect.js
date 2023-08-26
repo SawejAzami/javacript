1000363e3460e7409369ca2aa34c752cc90dd758
SawejAzami <122581170+SawejAzami@users.noreply.github.com>
2023-08-26 09:03:00 +0000

    synchronous javascript

---
+++ 23lect.js
@@ -0,0 +1,17 @@
+//************************  Synchronous javascript */
+
+//i) Synchronous means that it execute one by one
+// ii) single threaded  
+
+// execution context 
+        // -> execute one line of the code at a time
+        // -> console.log -1
+        // -> console.log -2                    // each operation wait for the last one to complete before executing
+        // call stack and memory heap
+
+
+
+        // blocking code                    Vs          Non blocking code
+
+        // block the flow of the program                does not block the execution 
+        // read file Synchronous                        read file Asynchonous
